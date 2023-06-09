609166c434e27bfb0aa76b8e0a7ceaba1f051642
denis <denispavlenko7610new@gmail.com>
2023-06-09 21:38:52 +0300

    ref

---
+++ Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace EraseRescycleBin
+{
+    class Constants
+    {
+        public const uint SHERB_NOCONFIRMATION = 0x00000001;
+        public const uint SHERB_NOPROGRESSUI = 0x00000002;
+        public const uint SHERB_NOSOUND = 0x00000004;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 ﻿using System.Runtime.InteropServices;
 
-class Program
+namespace EraseRescycleBin
 {
-    [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
-    static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, uint dwFlags);
-
-    static void Main(string[] args)
+    class Program
     {
-        const uint SHERB_NOCONFIRMATION = 0x00000001;
-        const uint SHERB_NOPROGRESSUI = 0x00000002;
-        const uint SHERB_NOSOUND = 0x00000004;
 
-        SHEmptyRecycleBin(IntPtr.Zero, null, SHERB_NOCONFIRMATION | SHERB_NOPROGRESSUI | SHERB_NOSOUND);
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, uint dwFlags);
+
+        static void Main(string[] args)
+        {
+            SHEmptyRecycleBin(IntPtr.Zero, null, Constants.SHERB_NOCONFIRMATION 
+                | Constants.SHERB_NOPROGRESSUI | Constants.SHERB_NOSOUND);
+        }
     }
 }
